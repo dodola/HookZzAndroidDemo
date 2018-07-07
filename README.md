34b1c03289ec932bb085d627b81793180abf620f
jmpews(AKA.zz) <jmpews@gmail.com>
2018-07-07 08:04:52 +0800

    initialize HookZz Android Demo

---
+++ README.md
@@ -0,0 +1,7 @@
+## HookZz Android Demo
+
+## Usage
+replace dummy file **`app/3rd_party/HookZz`** with **[HookZz](https://github.com/jmpews/HookZz)**.
+
+## Demo
+the demo file is **`app/src/main/cpp/hookzz_helper.cpp`**
---
+++ app/src/main/cpp/hookzz_helper.cpp
@@ -0,0 +1,64 @@
+//
+// Created by z on 2018/7/7.
+//
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "hookzz.h"
+
+#ifndef ZREG_MACRO
+#define ZREG_MACRO
+#if defined(__LP64__)
+#define ZREG(n) general.regs.x##n
+#else
+#define ZREG(n) general.regs.r##n
+#endif
+#endif
+
+/* demo for replace */
+
+size_t (*origin_fread)(void * ptr, size_t size, size_t nitems, FILE * stream);
+size_t (fake_fread)(void * ptr, size_t size, size_t nitems, FILE * stream) {
+    printf("[FileMonitor|fread|model|%p] >>> %ld, %ld\n", ptr, size, nitems);
+    return origin_fread(ptr, size, nitems, stream);
+}
+
+void hook_fread() { ZzReplace((void *)fread, (void *)fake_fread, (void **)&origin_fread); }
+
+
+/* demo for wrap */
+
+void open_pre_call(RegState *rs, ThreadStackPublic *tsp, CallStackPublic *csp, const HookEntryInfo *info) {
+    char *path = (char *)rs->ZREG(0);
+    int oflag  = (int)rs->ZREG(1);
+
+    switch (oflag) {
+        case O_RDONLY:
+            printf("[FileMonitor|open|R] >>> %s\n", path);
+            break;
+        case O_WRONLY:
+            printf("[FileMonitor|open|W] >>> %s\n", path);
+            break;
+        case O_RDWR:
+            printf("[FileMonitor|open|RW] >>> %s\n", path);
+            break;
+        default:
+            printf("[FileMonitor|open|-] >>> %s\n", path);
+            break;
+    }
+}
+
+void open_post_call(RegState *rs, ThreadStackPublic *tsp, CallStackPublic *csp, const HookEntryInfo *info) {
+}
+
+void hook_open() { ZzWrap((void *)open, open_pre_call, open_post_call); }
+
+
+
+
+__attribute__((constructor)) void hookzz_initialize() {
+    hook_open();
+    hook_fread();
+}
